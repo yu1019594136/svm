8eb2fdbc84bc169d5f265f550b7456e37edac80e
yu1019594136 <yu1019594136@163.com>
2015-07-30 15:00:22 +0800

    nothing

--- main.c
+++ main.c
@@ -37,33 +37,33 @@ int main(void)
     printf("Scaling train data...\n");
 
     /* 缩放训练样本 */
-//    Para_svm_scale.save_filename = Svm_all_filepath.range_filepath;
-//    Para_svm_scale.restore_filename = NULL;//save_filename和restore_filename参数必须至少有一个为NULL
-//    Para_svm_scale.data_set = Svm_all_filepath.train_data_filepath;
-//    Para_svm_scale.result_filename = Svm_all_filepath.train_data_scaled_filepath;
+    Para_svm_scale.save_filename = Svm_all_filepath.range_filepath;
+    Para_svm_scale.restore_filename = NULL;//save_filename和restore_filename参数必须至少有一个为NULL
+    Para_svm_scale.data_set = Svm_all_filepath.train_data_filepath;
+    Para_svm_scale.result_filename = Svm_all_filepath.train_data_scaled_filepath;
 
-//    if(main_svm_scale(&Para_svm_scale) == SUCCESS)
-//        printf("Train data scaled!\n");
-//    else
-//        printf("Train data scaling:something wrong! check the data format\n");
+    if(main_svm_scale(&Para_svm_scale) == SUCCESS)
+        printf("Train data scaled!\n");
+    else
+        printf("Train data scaling:something wrong! check the data format\n");
 
-//    if(Svm_all_filepath.test_data_filepath != NULL)
-//    {
-//        printf("Scaling test data...\n");
+    if(Svm_all_filepath.test_data_filepath != NULL)
+    {
+        printf("Scaling test data...\n");
 
-//        /* 缩放测试样本 */
-//        Para_svm_scale.save_filename = NULL;
-//        Para_svm_scale.restore_filename = Svm_all_filepath.range_filepath;//save_filename和restore_filename参数必须至少有一个为NULL
-//        Para_svm_scale.data_set = Svm_all_filepath.test_data_filepath;
-//        Para_svm_scale.result_filename = Svm_all_filepath.test_data_scaled_filepath;
+        /* 缩放测试样本 */
+        Para_svm_scale.save_filename = NULL;
+        Para_svm_scale.restore_filename = Svm_all_filepath.range_filepath;//save_filename和restore_filename参数必须至少有一个为NULL
+        Para_svm_scale.data_set = Svm_all_filepath.test_data_filepath;
+        Para_svm_scale.result_filename = Svm_all_filepath.test_data_scaled_filepath;
 
-//        if(main_svm_scale(&Para_svm_scale) == SUCCESS)
-//            printf("Test data scaled!\n");
-//        else
-//            printf("Test data scaling:something wrong! check the data format\n");
-//    }
-//    else
-//        printf("Warning: No test data filepath, test_data scaling cancle!\n");
+        if(main_svm_scale(&Para_svm_scale) == SUCCESS)
+            printf("Test data scaled!\n");
+        else
+            printf("Test data scaling:something wrong! check the data format\n");
+    }
+    else
+        printf("Warning: No test data filepath, test_data scaling cancle!\n");
 
 
 /************************** 格点搜索参数 **************************/
@@ -79,34 +79,34 @@ int main(void)
 //        printf("aco_search() failed. something wrong!\n");
 
 /************************** 交叉验证 or 训练模型 **************************/
-//    if(Para_svm_train.cross_validation)//如果参数中选择了交叉验证那么.....
-//        printf("Cross_validation...\n");
-//    else
-//        printf("Training ...\n");
+    if(Para_svm_train.cross_validation)//如果参数中选择了交叉验证那么.....
+        printf("Cross_validation...\n");
+    else
+        printf("Training ...\n");
 
-//    if((main_svm_train(&Para_svm_train)) == SUCCESS)
-//        printf("Done!\n");
-//    else
-//        printf("Something wrong!\n");
+    if((main_svm_train(&Para_svm_train)) == SUCCESS)
+        printf("Done!\n");
+    else
+        printf("Something wrong!\n");
 
-//    if(Para_svm_train.cross_validation)//如果参数中选择了交叉验证那么此处输出正确率
-//        printf("correct: %d, total: %d\n",result_cross_validation_accuracy.correct, result_cross_validation_accuracy.total);
+    if(Para_svm_train.cross_validation)//如果参数中选择了交叉验证那么此处输出正确率
+        printf("correct: %d, total: %d\n",result_cross_validation_accuracy.correct, result_cross_validation_accuracy.total);
 
 /************************** 测试样本预测 **************************/
-//    if(Svm_all_filepath.test_data_filepath != NULL)
-//    {
-//        printf("Predicting...\n");
+    if(Svm_all_filepath.test_data_filepath != NULL)
+    {
+        printf("Predicting...\n");
 
-//        if((main_svm_predict(&Para_svm_predict)) == SUCCESS)
-//            printf("Done!\n");
-//        else
-//            printf("Something wrong!\n");
+        if((main_svm_predict(&Para_svm_predict)) == SUCCESS)
+            printf("Done!\n");
+        else
+            printf("Something wrong!\n");
 
-//        //预测，打印正确率
-//        printf("correct: %d, total: %d\n",result_predict_accuracy.correct,result_predict_accuracy.total);
-//    }
-//    else
-//        printf("Warning: No test data filepath, test data predicting cancle!\n");
+        //预测，打印正确率
+        printf("correct: %d, total: %d\n",result_predict_accuracy.correct,result_predict_accuracy.total);
+    }
+    else
+        printf("Warning: No test data filepath, test data predicting cancle!\n");
 
 
 
@@ -129,24 +129,37 @@ int main(void)
 void para_config()
 {
     /* 配置缩放参数 */
-    Para_svm_scale.l = 0.0;
+    Para_svm_scale.l = -1.0;
     Para_svm_scale.u = 1.0;
     Para_svm_scale.y_scaling = 0;
     Para_svm_scale.y_lower = 0.0;
     Para_svm_scale.y_upper = 0.0;
     /* 手动配置 */
-    Svm_all_filepath.train_data_filepath = "C:\\UCI_database\\Letter Recognition Data Set\\ABC\\ABC.txt.train";//训练文件不能为NULL，"/home/zhouyu/UCI_database/wine/wine.txt.train"
-    Svm_all_filepath.test_data_filepath = "C:\\UCI_database\\Letter Recognition Data Set\\ABC\\ABC.txt.test";//NULL     如果没有测试文件，此处给NULL"/home/zhouyu/UCI_database/wine/wine.txt.test"
+    Svm_all_filepath.train_data_filepath = "/home/zhouyu/UCI_database/test/2_bainianwanjiu_4features.txt.train";//"C:\\UCI_database\\Letter Recognition Data Set\\ABC\\ABC.txt.train"训练文件不能为NULL，
+    Svm_all_filepath.test_data_filepath = "/home/zhouyu/UCI_database/test/2_bainianwanjiu_4features.txt.test";//NULL  "C:\\UCI_database\\Letter Recognition Data Set\\ABC\\ABC.txt.test"   如果没有测试文件，此处给NULL
+    /*
+     * 1_luzhoulaojiao
+     * 2_bainianwanjiu
+     * 3_daohuaxiang
+     * 4_qingjiu
+     * 5_langjinjiu
+     * 6_hongjinjiu
+     * 7_mianzhudaqu
+     * 8_niulangshan
+     * 9_jiujingrongye
+     *
+     */
+
     /* 程序自动按规则自动配置 */
     filepath_config(&Svm_all_filepath);
 
     /* 配置训练参数 */
-    Para_svm_train.svm_train_parameter.svm_type = C_SVC;//C_SVC, NU_SVC, ONE_CLASS, EPSILON_SVR, NU_SVR,选择一个参数
+    Para_svm_train.svm_train_parameter.svm_type = ONE_CLASS;//C_SVC, NU_SVC, , EPSILON_SVR, NU_SVR,选择一个参数
     Para_svm_train.svm_train_parameter.kernel_type = RBF;//LINEAR, POLY, RBF, SIGMOID, PRECOMPUTED, 选择一个参数
     Para_svm_train.svm_train_parameter.degree = 3;
-    Para_svm_train.svm_train_parameter.gamma = 3.201500;//0.1;//0代表默认
+    Para_svm_train.svm_train_parameter.gamma = 0.004645;//0.1;//0代表默认
     Para_svm_train.svm_train_parameter.coef0 = 0;
-    Para_svm_train.svm_train_parameter.nu = 0.5;//
+    Para_svm_train.svm_train_parameter.nu = 0.000001;//
     Para_svm_train.svm_train_parameter.cache_size = 100;
     Para_svm_train.svm_train_parameter.C = 	4.323161;//1;
     Para_svm_train.svm_train_parameter.eps = 1e-3;
@@ -156,7 +169,7 @@ void para_config()
     Para_svm_train.svm_train_parameter.nr_weight = 0;//0表示不设置权重，n表示有n个类别要设置权重系数，n个类别以及对应的权重系数由本文件开头处的数组weight_label和weight设定,注意其数组长度必须和n相等
     Para_svm_train.svm_train_parameter.weight_label = weight_label;//该数组在本文件开头处初始化
     Para_svm_train.svm_train_parameter.weight = weight;//该数组在本文件开头处初始化
-    Para_svm_train.quiet_mode = 1;//0: outputs;  !0: no outputs
+    Para_svm_train.quiet_mode = 0;//0: outputs;  !0: no outputs
     Para_svm_train.cross_validation = 0;// 5;// 0;//输入0表示不进行交叉验证，输入n表示进行n折交叉验证（注意n必须大于2）
     Para_svm_train.training_set_file = Svm_all_filepath.train_data_scaled_filepath;
     Para_svm_train.model_file = Svm_all_filepath.model_filepath;
@@ -169,13 +182,13 @@ void para_config()
     Para_svm_predict.output_file = Svm_all_filepath.predict_accuracy_filepath;
 
     /* 格点搜索相关参数(分类器类型和核函数类型在训练参数中配置) */
-    Para_grid_search.d1_begin = 1.678748;//底数为2，下同,搜索参数时第一个参数d1必须是核函数参数g，第二个参数d2是分类器参数(nu or C)
-    Para_grid_search.d1_end = 2;//10;
-    Para_grid_search.d1_step = 0.01;
-    Para_grid_search.d2_begin = 2.11208;//注意nu参数取值范围（0,1）开区间
-    Para_grid_search.d2_end = 2.5;
+    Para_grid_search.d1_begin = -10;//底数为2，下同,搜索参数时第一个参数d1必须是核函数参数g，第二个参数d2是分类器参数(nu or C)
+    Para_grid_search.d1_end = 5;//10;
+    Para_grid_search.d1_step = 0.1;
+    Para_grid_search.d2_begin = -10;//注意nu参数取值范围（0,1）开区间
+    Para_grid_search.d2_end = -0.01;
     Para_grid_search.d2_step = 0.01;//1;
-    Para_grid_search.v_fold = 4;
+    Para_grid_search.v_fold = 3;
     Para_grid_search.flag_predict = 1;// 1,表示每个参数在交叉验证之后，再进行模型训练，然后在对测试样本做预测；0表示不做预测
     Para_grid_search.output_file = Svm_all_filepath.grid_search_result;
 
